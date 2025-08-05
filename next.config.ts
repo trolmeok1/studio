d0d01ea7c190081737c654763c5b351b5a77da4a
Trol Me (Trol Me) <elian0606espin@gmail.com>
2025-08-05 17:15:20 +0000

    me referia al que abre eso

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    buildActivity: false
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
