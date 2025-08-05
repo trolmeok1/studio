df39960464e03f2475589c490c9f3492e98e2e78
Trol Me (Trol Me) <elian0606espin@gmail.com>
2025-08-05 17:19:49 +0000

    me llego una alerta por la API del proyecto que esta publicado en otro l

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp, type FirebaseOptions } from 'firebase/a
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig: FirebaseOptions = {
-  projectId: "teamlink-hub-qennu",
-  appId: "1:859037440361:web:17c901219ba23c393a85f8",
-  storageBucket: "teamlink-hub-qennu.firebasestorage.app",
-  apiKey: "AIzaSyAtrkvOubUuW7931jYcmBllaMJ4eBpiUkE",
-  authDomain: "teamlink-hub-qenqu.firebaseapp.com",
-  messagingSenderId: "859037440361",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
